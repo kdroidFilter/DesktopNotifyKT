39ff451b1a7507082d685e098f21c7890e582f76
Elie G. <elyahou.hadass@gmail.com>
2025-05-09 10:41:30 +0300

    Update Demo

--- demo/composeApp/src/jvmMain/kotlin/Main.kt
+++ demo/composeApp/src/jvmMain/kotlin/Main.kt
@@ -2,12 +2,10 @@ package io.github.kdroidfilter.knotify.demo
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
 import io.github.kdroidfilter.knotify.builder.AppConfig
 import io.github.kdroidfilter.knotify.builder.NotificationInitializer
 import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.Res
 import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.compose
-import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.kdroid
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
