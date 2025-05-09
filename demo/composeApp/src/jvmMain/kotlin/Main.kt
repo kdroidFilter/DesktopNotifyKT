983c53590f864068297879ab63a63a565b2fac62
Elie G. <elyahou.hadass@gmail.com>
2025-05-09 10:41:21 +0300

    Update Demo

--- demo/composeApp/src/jvmMain/kotlin/Main.kt
+++ demo/composeApp/src/jvmMain/kotlin/Main.kt
@@ -6,15 +6,19 @@ import androidx.compose.ui.window.application
 import io.github.kdroidfilter.knotify.builder.AppConfig
 import io.github.kdroidfilter.knotify.builder.NotificationInitializer
 import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.Res
+import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.compose
+import io.github.kdroidfilter.knotify.demo.composeapp.generated.resources.kdroid
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Compose Native Notification Demo") {
+    Window(onCloseRequest = ::exitApplication, title = "Compose Native Notification Demo", icon = painterResource(Res.drawable.compose)) {
+
         NotificationInitializer.configure(
             AppConfig(
-                appName = "My awesome app",
-                smallIcon = Res.getUri("drawable/kdroid.png"),
+                appName =  window.title,
+                smallIcon = Res.getUri("drawable/compose.png"),
             )
         )
         App()
